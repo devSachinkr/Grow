d594e77ab64b4cbcf377bf0bcd646ee0a0e2c59d
devSachinkr <sachin67.dev@gmail.com>
2024-04-16 17:34:19 +0530

    changes in routes

--- src/app/api/uploadthing/route.ts
+++ src/app/api/uploadthing/route.ts
@@ -2,6 +2,7 @@ import { createRouteHandler } from "uploadthing/server";
 
 import { ourFileRouter } from "./core";
 
-export const { GET, POST } = createRouteHandler({
+export const { POST } = createRouteHandler({
   router: ourFileRouter,
 });
+
